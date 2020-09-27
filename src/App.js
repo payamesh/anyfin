7c969a1f0ab095a29073b86a5c19bd8cc39fe27d
payam esh <payam.eshtiagh@gmail.com>
2020-09-27 16:32:35 +0200

    done

--- src/App.js
+++ src/App.js
@@ -14,12 +14,14 @@ function App() {
   };
 
   useEffect(() => {
+    //get all countries from restcountries API
     fetch("https://restcountries.eu/rest/v2/all")
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
       });
 
+    //get currency data from fixer.io API
     fetch(
       "http://data.fixer.io/api/latest?access_key=b872f3b7a4fefeb94a3a03e1a9896258"
     ) //since I wasnt able to use "&base=SEK" without paying for the API, I convert it in Country.js
@@ -30,6 +32,7 @@ function App() {
   }, []);
 
   useEffect(() => {
+    //make sure search results are up to date
     if (countries) {
       const results = countries.filter((countries) =>
         countries.name.toLowerCase().includes(searchWord.toLowerCase())
@@ -45,15 +48,16 @@ function App() {
           {searchResult && (
             <div className={styles.countryContainer}>
               {searchResult.slice(0, loadMore).map((
-                country //limited to 8 results
+                country //limited to 8 results at first for cleanliness
               ) => (
                 <Country
                   key={country.name}
-                  sek={currencies ? currencies.rates.SEK : 10.62} //get the rate for sek
+                  sek={currencies.rates ? currencies.rates.SEK : 10.62} //get the rate for sek
                   country={country}
+                  //use country-code to find its currency in currency object
                   countryCurrency={
                     currencies.rates[country?.currencies[0]?.code]
-                  } //use country-code to find its currency in currency object
+                  }
                 />
               ))}
             </div>
@@ -61,6 +65,7 @@ function App() {
 
           <button
             className={styles.showMore}
+            //add 8 more results, could do a separate func for this, but felt redundant
             onClick={() => setLoadMore(loadMore + 8)}
           >
             Load more
